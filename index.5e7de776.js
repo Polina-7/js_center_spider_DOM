0bd6861ea12f0db20ca580c600bc2a90935068b1
Polina Yermak <polyousok@gmail.com>
2025-04-02 15:40:42 +0300

    make build

---
+++ index.5e7de776.js
@@ -0,0 +1,2 @@
+window.addEventListener("load",function(){var t=document.querySelector(".wall"),e=document.querySelector(".spider");if(t&&e){var i=t.clientWidth,l=t.clientHeight,n=e.clientWidth,c=e.clientHeight;e.style.position="absolute",e.style.left="".concat((i-n)/2,"px"),e.style.top="".concat((l-c)/2,"px")}});
+//# sourceMappingURL=index.5e7de776.js.map
